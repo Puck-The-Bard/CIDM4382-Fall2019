345ff71adbb6dbe489d2a5cd8f1b3a8124a255d1
jeff <ahuimanu@gmail.com>
2019-09-08 19:56:55 -0500

    updated Nicknames to use Big Huge Thesaurus API"

--- Nickname/app_server/routes/index.js
+++ Nickname/app_server/routes/index.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* CONTROLLERS */
-const homepageController = (req, res) => {
-  res.render('index', { title: 'Nickname Generator' });
-};
+const controllerMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', homepageController);
+router.get('/', controllerMain.index);
+
+router.post('/', controllerMain.nickname);
 
 module.exports = router;
